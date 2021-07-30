c5930150cd568022d2327aa1a0558ef219f1e44c
Mia-v <vasilyeva.mariya@gmail.com>
2021-07-30 23:53:10 +0200

    Add styles folder. Add css and style loaders to webpack configuration file.

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './styles/index.css';
+import Watermark from './assets/watermark.svg';
--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   mode: 'development',
+  context: path.resolve(__dirname, 'src'),
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './index.html'
     }),
     new CleanWebpackPlugin()
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 }
