54ac4e09bc412bca7efe8585d4e6bbe4a37a7dbf
Mia-v <vasilyeva.mariya@gmail.com>
2021-08-03 23:36:34 +0200

    Add bunch of webpack loaders. Add an favicon. Add some content of ui-kit and style.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import './styles/index.css';
+import './styles/main.scss';
 import Watermark from './assets/watermark.svg';
+import './assets/favicons/favicon.ico';
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,83 @@
 const path = require('path'); 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 module.exports = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   mode: 'development',
   context: path.resolve(__dirname, 'src'),
+  devServer: {
+    port: 3000
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html'
+      template: './index.html',
+      favicon: "./assets/favicons/favicon.ico" 
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),    
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './src/assets/favicons/favicon.ico'),
+          to: path.resolve(__dirname, 'dist')
+        }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    })
   ],
+
   module: {
     rules: [
       {
+        test: /\.[html|pug]$/i,
+        use: ['html-loader', 'pug-html-loader'],
+      },
+      {
         test: /\.css$/, 
-        use: ['style-loader', 'css-loader']
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              
+            },
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/, 
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              
+            },
+          },
+          "css-loader",
+          "sass-loader"
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(svg|png|ico|xml|json)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|woff|eot|svg)$/i,
+        type: 'asset/resource',
       }
     ]
   }
